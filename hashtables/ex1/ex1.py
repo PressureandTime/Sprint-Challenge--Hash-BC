ca85754b110a20dc6d7d684b6355aa0f712aed31
Petar Scepanovic <hiborija@gmail.com>
2020-03-13 09:48:50 +0100

    finished the ex1.py, tests are passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,18 +1,22 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_remove,
+                        hash_table_retrieve, hash_table_resize)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
 
+    for j in range(len(weights)):
+        different = limit - weights[j]
+        different_index = hash_table_retrieve(ht, different)
+        if different_index:
+            if j < different_index:
+                return different_index, j
+            else:
+                return j, different_index
     return None
 
 
